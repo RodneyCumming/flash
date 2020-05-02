72f4bdfa2eb81007144e5e595962cd5051723066
RodneyCumming <rodneycumming@gmail.com>
2020-05-02 13:33:19 +1200

    add basic react setup

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styled from './Home.styled';
+
+function Home() {
+  return (
+    <Styled.Home className="App">
+      Flash App
+    </Styled.Home>
+  );
+}
+
+export default Home;
---
+++ src/Styles/colours.js
@@ -0,0 +1,12 @@
+export const fontColours = {
+  alto: "#d3d3d3",
+};
+
+export const backgroundColours = {
+  jaggedIce: "#cbe0ec",
+  darkMirage: "#1a212f",
+};
+
+export const boxShadowColours = {
+  // lightBottom: `0 6px 7px 2px rgba(0, 0, 0, 0.2)`,
+};
